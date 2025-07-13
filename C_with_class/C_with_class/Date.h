c765d13bd1aa0fc13ceaf52252831edffee6e007
xpr123 <3424286550@qq.com>
2025-07-13 22:10:28 +0800

    日期类

--- C_with_class/C_with_class/Date.h
+++ C_with_class/C_with_class/Date.h
@@ -3,6 +3,17 @@
 using namespace std;
 class Date
 {
+	//单个传递
+	//cout << d1;
+	//friend void operator<<(ostream& out, const Date& d);
+	
+	//连续赋值
+	//cout << d1 << d2 << d3;
+	//友元函数是声明,没有受到访问限定符的限制
+	friend ostream& operator<<(ostream& out, const Date& d);
+
+	//流提取
+	friend istream& operator>>(istream& out, Date& d);
 public:
 	//构造函数
 	Date(int year = 1, int month = 1, int day = 1);
@@ -28,8 +39,25 @@ public:
 	Date& operator-=(int day);
 
 	Date operator-(int day);
+	//前置--
+	Date& operator--();
+
+	//后置--
+	Date operator--(int);
+
+	int operator-(const Date& d);
+	//流插入不能写成成员函数,因为Date对象默认占用了第1个参数,做了左操作数,不符合使用习惯
+	//d1 << cout;
+	//void operator<<(ostream& out);
+	//int GetYear();
+	//int GetMonth();
+	//int GetDay();
 private:
 	int _year;
 	int _month;
 	int _day;
 };
+
+
+//全局变量
+//void operator<<(ostream& out, Date& d);
