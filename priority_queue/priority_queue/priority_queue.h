1a320dcd6ef131439337510c73a52c0ceac81506
xpr123 <3424286550@qq.com>
2025-10-02 21:51:08 +0800

    优先级队列

--- priority_queue/priority_queue/priority_queue.h
+++ priority_queue/priority_queue/priority_queue.h
@@ -1,1 +1,122 @@
 #pragma once
+#include<iostream>
+#include<vector>
+#include"Date.h"
+using namespace std;
+template<class T>
+class Less
+{
+public:
+	bool operator()(const T& x, const T& y)
+	{
+		return x < y;
+	}
+};
+template<class T>
+class Greater
+{
+public:
+	bool operator()(const T& x, const T& y)
+	{
+		return x > y;
+	}
+};
+namespace prior
+{
+	template<class T, class Container = vector<T>, class Compare = less<T>>
+	class priority_queue
+	{
+	private:
+		void AdjustDown(int parent)
+		{
+			Compare com;
+			//找左右孩子大的那一个
+			int child = parent * 2 + 1;
+			while (child < _con.size())
+			{
+				if (child + 1 < _con.size() && com(_con[child + 1], _con[child]))
+				{
+					child++;
+				}
+				if (com(_con[parent], _con[child]))
+				{
+					swap(_con[child], _con[parent]);
+					parent = child;
+					child = parent * 2 + 1;
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+		void AdjustUp(int child)
+		{
+			Compare com;
+			int parent = (child - 1) / 2;
+			while (child > 0)
+			{
+				if (com(_con[child], _con[parent]))
+				{
+					swap(_con[child], _con[parent]);
+					child = parent;
+					parent = (child - 1) / 2;
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+	public:
+		priority_queue()
+		{
+		}
+		template<class InputIterator>
+		priority_queue(InputIterator first, InputIterator last)
+		{
+			while (first != last)
+			{
+				_con.push_back(*first);
+				first++;
+			}
+			//建堆
+			for (int i = (_con.size() - 1 - 1) / 2; i >= 0; i--)
+			{
+				AdjustDown(i);
+			}
+		}
+		void pop()
+		{
+			swap(_con[0], _con[_con.size() - 1]);
+			_con.pop_back();
+			AdjustDown(0);//从根开始
+		}
+		void push(const T& x)
+		{
+			_con.push_back(x);
+			AdjustUp(_con.size() - 1);
+		}
+		const T& top()
+		{
+			return _con[0];
+		}
+		bool empty()
+		{
+			return _con.empty();
+		}
+		size_t size()
+		{
+			return _con.size();
+		}
+	private:
+		Container _con;
+	};
+	struct LessPDate
+	{
+		bool operator()(const Date* p1, const Date* p2)
+		{
+			return *p1 < *p2;
+		}
+	};
+};
--- priority_queue/priority_queue/test.cpp
+++ priority_queue/priority_queue/test.cpp
@@ -1,12 +1,14 @@
-#include<iostream>
-#include<queue>
-using namespace std;
-void test()
+//#include<iostream>
+//#include<queue>
+//using namespace std;
+#include"priority_queue.h"
+using namespace prior;
+void test1()
 {
 	//priority_queue<int, deque<int>, greater<int>> pq;
 
 	//小堆
-	priority_queue<int, vector<int>,greater<int>> pq;
+	priority_queue<int, vector<int>,Greater<int>> pq;
 	
 	//大堆
 	//priority_queue<int> pq;
@@ -22,9 +24,43 @@ void test()
 	}
 	cout << endl;
 }
-
+void test2()
+{
+	//priority_queue<Date> pq;//
+	priority_queue<Date,vector<Date>,greater<Date>> pq;
+	pq.push(Date(2025, 10, 2));
+	pq.push(Date(2025, 12, 2));
+	pq.push(Date(2025, 11, 2));
+	while (!pq.empty())
+	{
+		cout << pq.top() << " ";
+		pq.pop();
+	}
+	cout << endl;
+}
+void test3()
+{
+	//priority_queue<Date*> pq;
+	priority_queue<Date*,vector<Date*>,LessPDate> pq;
+	pq.push(new Date(2025, 10, 2));
+	pq.push(new Date(2025, 12, 2));
+	pq.push(new Date(2025, 11, 2));
+	while (!pq.empty())
+	{
+		cout << *pq.top() << " ";
+		pq.pop();
+	}
+	cout << endl;
+}
 int main()
 {
-	test();
+	//test1(); //内置类型
+	test3();   //自定义类型
+	//仿函数/函数对象
+	//类的对象也可以像函数一样使用
+	//Less<int> lessfunc;
+	//cout << lessfunc(1, 2) << endl;
+	//cout << lessfunc.operator()(1, 2) << endl;
 	return 0;
 }
+
